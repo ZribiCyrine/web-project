a32f5cef510082c87a8cb3192685ad4a7904239d
Cyrine <cyrinezribi23"@gmail.com>
2024-02-01 22:31:56 +0100

    final

--- event-management/src/main.ts
+++ event-management/src/main.ts
@@ -6,12 +6,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // Activez CORS pour toutes les routes et origines
   app.enableCors();
   
-  // Ou pour une configuration plus spécifique :
   app.enableCors({
-    origin: ['http://localhost:4200', 'http://localhost:55535'], // ou un tableau d'origines valides
+    origin: ['http://localhost:4200', 'http://localhost:55535'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
   });
